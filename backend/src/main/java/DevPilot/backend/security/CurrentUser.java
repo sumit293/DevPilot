379120300a0caff116cbedf50ab783ce03c8ae0b
sumi <sumitwani310@gmail.com>
2026-09-24 23:04:39 +0530

    main  file

---
+++ backend/src/main/java/DevPilot/backend/security/CurrentUser.java
@@ -0,0 +1,11 @@
+package DevPilot.backend.security;
+
+import org.springframework.stereotype.Component;
+
+@Component 
+public class CurrentUser {
+    Public AppUserPrinciple require(){
+        
+    }
+    // main file 
+}
